b0770e95ef3964139ad3f13fc61739dfbba62a12
amit kumar <amit.ap1924@gmail.com>
2020-10-23 23:02:27 +0530

    google sign In

---
+++ silk/src/main/java/com/silk/ui/GoogleLoginActivity.kt
@@ -0,0 +1,77 @@
+package com.silk.ui
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_google_login.sign_in_button as signInButton
+
+
+class GoogleLoginActivity : AppCompatActivity() {
+    private val RC_SIGN_IN = 1000
+    private val TAG = "GoogleLoginActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google_login)
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if(account != null) {
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
+        signInButton.setOnClickListener {
+            startActivityForResult(mGoogleSignInClient.signInIntent, RC_SIGN_IN)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account =
+                completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            setResult(RESULT_OK)
+            finish()
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+            Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show()
+
+        }
+    }
+}
--- silk/src/main/java/com/silk/ui/SplashActivity.kt
+++ silk/src/main/java/com/silk/ui/SplashActivity.kt
@@ -1,9 +1,11 @@
 package com.silk.ui
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Toast
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +13,22 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+            startActivityForResult(Intent(this, GoogleLoginActivity::class.java), 100)
         }, 3000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode) {
+            100 -> {
+                when(resultCode) {
+                    Activity.RESULT_OK ->
+                        startActivity(Intent(this, MainActivity::class.java))
+                    Activity.RESULT_CANCELED ->
+                        Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
